910a37e0f1eec84d005951d5fe9ebb3c1ebd5cdc
ledinhloc <ledinhloc7@gmail.com>
2026-04-30 23:43:02 +0700

    feat(): update model user

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -1,8 +1,11 @@
-
 import 'package:json_annotation/json_annotation.dart';
 
+import '../../domain/enum/activity_level.dart';
 import '../../domain/entities/user.dart';
 
+part 'user_model.g.dart';
+
+@JsonSerializable(explicitToJson: true)
 class UserModel {
   final int id;
   final String username;
@@ -15,9 +18,11 @@ class UserModel {
   final String provider;
   final String? avatarUrl;
   final String? backgroundUrl;
+  final ActivityLevel? activityLevel;
   final List<String>? allergies;
   final List<String>? eatingPreferences;
   final List<String>? dietaryPreferences;
+  final List<String>? healthConditions;
 
   final String? accessToken;
 
@@ -34,99 +39,53 @@ class UserModel {
     required this.provider,
     this.avatarUrl,
     this.backgroundUrl,
+    this.activityLevel,
     this.allergies,
     this.eatingPreferences,
     this.dietaryPreferences,
+    this.healthConditions,
     this.accessToken,
     this.refreshToken,
-    this.fcmToken
+    this.fcmToken,
   });
 
-  // factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
-  factory UserModel.fromJson(Map<String, dynamic> json) {
-    return UserModel(
-      id: json['id'],
-      username: json['username'] ,
-      email: json['email'],
-      role: json['role'] ,
-      dob: json['dob']?.toString(),
-      gender: json['gender']?.toString(),
-      address: json['address'] != null
-          ? AddressModel.fromJson(Map<String, dynamic>.from(json['address']))
-          : null,
-      provider: json['provider'],
-      avatarUrl: json['avatarUrl']?.toString(),
-      backgroundUrl: json['backgroundUrl']?.toString(),
-      allergies: List<String>.from(json['allergies'] ?? []),
-      eatingPreferences: List<String>.from(json['eatingPreferences'] ?? []),
-      dietaryPreferences: List<String>.from(json['dietaryPreferences'] ?? []),
-      accessToken: json['accessToken'],
-      refreshToken: json['refreshToken'],
-      fcmToken: json['fcmToken']
-    );
-  }
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
 
   /* Chuyen user thanh json */
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'username': username,
-      'email': email,
-      'role': role,
-      'dob': dob,
-      'gender': gender,
-      'address': address?.toJson(),
-      'provider': provider,
-      'avatarUrl': avatarUrl,
-      'backgroundUrl': backgroundUrl,
-      'allergies': allergies,
-      'eatingPreferences': eatingPreferences,
-      'dietaryPreferences': dietaryPreferences,
-      'accessToken': accessToken,
-      'refreshToken': refreshToken,
-      'fcmToken': fcmToken
-    };
-  }
+  Map<String, dynamic> toJson() => _$UserModelToJson(this);
 
   User toEntity() => User(
     id: id,
     username: username,
     email: email,
-    role: role ,
+    role: role,
     dob: dob,
     gender: gender,
     address: address?.toEntity(),
     provider: provider,
     avatarUrl: avatarUrl,
     backgroundUrl: backgroundUrl,
+    activityLevel: activityLevel,
     allergies: allergies,
     eatingPreferences: eatingPreferences,
     dietaryPreferences: dietaryPreferences,
+    healthConditions: healthConditions,
   );
 }
 
+@JsonSerializable()
 class AddressModel {
   final String city;
   final String street;
 
   AddressModel({required this.city, required this.street});
 
-  /* chuyen thanh AddressModel */
-  // factory AddressModel.fromJson(Map<String, dynamic> json) => _$AddressModelFromJson(json);
-  factory AddressModel.fromJson(Map<String, dynamic> json) {
-    return AddressModel(
-      city: json['city'] != null ? json['city'].toString() : '',
-      street: json['street'] != null ? json['street'].toString() : '',
-    );
-  }
+  factory AddressModel.fromJson(Map<String, dynamic> json) =>
+      _$AddressModelFromJson(json);
 
   /* Chuyen address thanh json*/
-  // Map<String, dynamic> toJson() => _$AddressModelToJson(this);
-  Map<String, dynamic> toJson() {
-    return  {
-      'city': city,
-      'street': street
-    };
-  }
+  Map<String, dynamic> toJson() => _$AddressModelToJson(this);
+
   Address toEntity() => Address(city: city, street: street);
 }
